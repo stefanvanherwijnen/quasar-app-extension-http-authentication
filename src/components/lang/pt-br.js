71293ff422e40add27397db498c166c60a8478a9
Douglas <machadoug@users.noreply.github.com>
2020-08-20 07:44:00 -0300

    Update pt-br.js

--- src/components/lang/pt-br.js
+++ src/components/lang/pt-br.js
@@ -16,7 +16,7 @@ export default {
         'Favor acessar seu email e validar sua conta primeiro..',
       invalidCredentials: 'Endereço de email ou senha estão incorretos.',
       rememberMe: 'Lembre de mim',
-      registerMessage: 'Você ainda não crioou uma conta?',
+      registerMessage: 'Você ainda não criou uma conta?',
       register: 'Cadastre-se aqui.'
     },
     register: {
@@ -27,13 +27,13 @@ export default {
         'Endereço de email já cadastrado. Favor verificar seu email para validar o cadastro.',
       accountCreated:
         'Favor verificar seu email para validar o cadastro. As vezes este processo pode levar até 10 minutos. Favor verificar sua caixa de spam caso ainda não recebeu o email.',
-      checkEmail: (email) => `Você quer efetuar o cadastro com o endereço de email ${email}, correto?`,
+      checkEmail: (email) => `Favor confirmar se o endereço de email ${email} está correto.`,
       error: 'Algo deu errado.'
     },
     verification: {
       success: 'Sua conta foi verificada e você pode agora fazer o login.',
       failed: 'Verificação da conta falhou ou a conta já foi verificada. Tente fazer o login.'
-    },
+    }, 
     logout: {
       confirmation: 'Tem certeza que deseja sair?',
       confirm: 'Confirmar',
